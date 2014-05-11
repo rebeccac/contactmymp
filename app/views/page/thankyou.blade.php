a5a75ae8bdd8502826f72e3f6171c1922f095dfd
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-11 15:28:40 +1000

    Thank you page

--- app/views/page/thankyou.blade.php
+++ app/views/page/thankyou.blade.php
@@ -2,12 +2,17 @@
 
 @section('content')
    <h2 class="sub-heading">Thank you for your feedback</h2>
-   Thank you!
-   {{ $data['name'] }}
-   <br>
-   {{ $data['email'] }}
-   <br>
-   {{ $data['subject'] }}
-   <br>
-   {{ $data['message'] }}
+   <p>
+      Thank you!
+      <br>
+      You have sent the following feedback:
+      <br>
+      <strong>Name: </strong>{{ $data['name'] }}
+      <br>
+      <strong>Email: </strong>{{ $data['email'] }}
+      <br>
+      <strong>Subject: </strong>{{ $data['subject'] }}
+      <br>
+      <strong>Message: </strong>{{ $data['message'] }}
+   </p>
 @stop
