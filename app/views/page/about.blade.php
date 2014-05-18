8b04e4efbbdca52851afcc8328043dca51061e33
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-18 16:13:10 +0930

    About Us Page

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.index')
 
 @section('content')
-   <h1>This is the About Us page</h1>
+   <h2 class="sub-heading">About Us</h2>
+
+   <div class="text-box">
+      Contact My MP is not affiliated with the Australian Parliament or any Australian politicians.
+      <br><br>
+      It is designed to faciliate easy communication between constituents and their MPs and Senators.
+      <br><br>
+      If you have any comments, we would love to hear from you via our <a href="{{URL::to('contact')}}">contact</a> form.
+   </div>
+
 @stop
