bc9a67021a372bfc8b12a84fb2b6a56259b30f8b
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-21 16:45:30 +0930

    get mpemail & senatoremail routes

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,9 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	public function index()
 	{
-		return View::make('hello');
+		return View::make('index', array('page_title' => "Contact My MP"));
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,14 @@
 <?php
 
-
-Route::get('/', function()
-{
-	return View::make('index')->with('page_title', "Contact My MP");
-});
-
+Route::get('/', array('uses' => 'HomeController@index'));
 Route::get('about', array('as' => 'about', 'uses' => 'PageController@getAbout'));
 Route::get('contact', array('as' => 'contact', 'uses' => 'PageController@getContact'));
 Route::post('contact', array('as' => 'contact', 'uses' => 'PageController@postContact'));
 Route::get('thankyou', array('as', 'thankyou', 'uses' => 'PageController@getThankYou'));
 Route::post('mpemail', array('as' => 'mpemail', 'uses' => 'MPController@mpemail'));
+Route::get('mpemail', array('uses' => 'HomeController@index'));
 Route::post('senatoremail', array('as' => 'senatoremail', 'uses' => 'SenatorController@senatoremail'));
+Route::get('senatoremail', array('uses' => 'HomeController@index'));
 Route::get('findpolitician', array('as' => 'findpolitician', 'uses' => 'PageController@findPolitician'));
 Route::post('postFindPolitician', 'PageController@postFindPolitician');
 Route::get('ministers', array('as' => 'ministers', 'uses' => 'PageController@getMinisters'));
