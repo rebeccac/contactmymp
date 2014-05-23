360926ee6867f5fd6778331f873bf1ceba137f3c
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-23 17:03:03 +0930

    About Us page

--- app/views/page/about.blade.php
+++ app/views/page/about.blade.php
@@ -4,11 +4,17 @@
    <h2 class="sub-heading">About Us</h2>
 
    <div class="text-page">
+      Your MP and Senators represent you in the Australian Parliament and you have the right to
+      contact them with any concerns or problems you might have.
+      We hope that <em>Contact My MP</em> will help to faciliate easy communication between constituents and
+      their representatives in the Federal Parliament.
+      <br><br>
+      Your emails to MPs and Senators are completely private and not accessible by anyone involved with <em>Contact My MP</em>.
+      For further information, please see the <a href="{{ URL::to('privacy') }}">Privacy Policy</a>.
+      <br><br>
       <em>Contact My MP</em> is not affiliated with the Australian Parliament or any Australian politicians.
       <br><br>
-      It is designed to faciliate easy communication between constituents and their MPs and Senators.
-      <br><br>
-      If you have any comments, we would love to hear from you via our <a href="{{URL::to('contact')}}">contact</a> form.
+      If you have any comments, we would love to hear from you via our <a href="{{URL::to('contact')}}">contact form</a>.
    </div>
 
 @stop
