8264e6d35e66cf31208a22097db5856490477acd
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-05-25 16:02:46 +0930

    Privacy Policy

--- app/views/page/privacy.blade.php
+++ app/views/page/privacy.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.index')
 @section('content')
    <h2 class="sub-heading">Privacy Policy</h2>
+
+   <div class="text-page">
+      This following document sets forth the Privacy Policy for the <em>Contact My MP</em> website, <a href="http://www.contactmymp.com">www.contactmymp.com</a>.
+      <br><br>
+      <em>Contact My MP</em> is committed to providing you with the best possible experience. <em>Contact My MP</em> is bound by the Privacy Act 1988 (Crh), which sets out a number of principles concerning the privacy of individuals.
+      <br><br>
+      <strong>Collection of your personal information</strong>
+      <br>
+      There are many aspects of the site which can be viewed without providing personal information. However, in order to send an email to your MP or Senator, you are required to submit personally identifiable information. This includes your name and email address. You must also provide a message subject and body. This information is used only in sending your email and is not accessible by anyone involved with <em>Contact My MP</em>.
+      <br><br>
+      <strong>Use of your personal information</strong>
+      <br>
+      For each visitor to reach the site, we expressively collect the following non-personally identifiable information, including but not limited to browser type, version and language, operating system, pages viewed while browsing the Site, page access times and referring website address. This collected information is used solely internally for the purpose of gauging visitor traffic, trends and delivering personalized content to you while you are at this Site.
+      <br><br>
+      From time to time, we may use customer information for new, unanticipated uses not previously disclosed in our privacy notice. If our information practices change at some time in the future we will use for these new purposes only, data collected from the time of the policy change forward will adhere to our updated practices.
+      <br><br>
+      <strong>Changes to this Privacy Policy</strong>
+      <br>
+      <em>Contact My MP</em> reserves the right to make amendments to this Privacy Policy at any time. If you have objections to the Privacy Policy, you should not access or use the Site.
+      <br><br>
+      <strong>Accessing Your Personal Information</strong>
+      <br>
+      You have a right to access your personal information, subject to exceptions allowed by law. If you would like to do so, please let us know. You may be required to put your request in writing for security reasons. <em>Contact My MP</em> reserves the right to charge a fee for searching for, and providing access to, your information on a per request basis.
+      <br><br>
+      <strong>Contacting us</strong>
+      <br>
+      <em>Contact My MP</em> welcomes your comments regarding this Privacy Policy. If you have any questions about this Privacy Policy and would like further information, please <a href="{{ URL::to('contact') }}">contact us</a>.
+   </div>
 @stop
